c22603f26dc9fc6ddbfb3f6a8b4fbaa34beaa113
AvadhutJadhav96 <avadhutjadhav966@gmail.com>
2025-03-20 21:34:53 +0530

    Added user routes with authentication middleware

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,15 +1,19 @@
 package routes
 
-import(
-	"github.com/AvadhutJadhav96/Go-JWT/controllers" 
-	"github.com/gin-gonic/gin"
-	"github.com/AvadhutJadhav96/Go-JWT/middleware" 	             
+import (
+	controller "github.com/AvadhutJadhav96/Go-JWT/controllers" // Import user-related controllers
+	"github.com/AvadhutJadhav96/Go-JWT/middleware" // Import authentication middleware
+	"github.com/gin-gonic/gin" // Import Gin framework for routing
 )
 
-func UserRoutes(incomingRoutes *gin.Engine){
+// UserRoutes defines routes for fetching user data, protected by authentication middleware.
+func UserRoutes(incomingRoutes *gin.Engine) {
+	// Apply authentication middleware to all user-related routes
 	incomingRoutes.Use(middleware.Authenticate())
-	//used to authenticate the token
 
-	incomingRoutes.GET("/users", controllers.GetUSsers())
-	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
+	// Route to get a list of all users
+	incomingRoutes.GET("/users", controller.GetUsers())
+
+	// Route to get details of a specific user by user ID
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
 }
